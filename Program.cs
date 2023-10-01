141c711f9ecbd4377494f4dbb6efb734defdfba6
Mita4et0 <124554897+Mita4et0@users.noreply.github.com>
2023-10-01 12:58:59 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,18 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Collections.Generic;
-using System.Linq;
-Console.WriteLine("Kolko produkta iskate");
-int n =int.Parse(Console.ReadLine());
-List <string> produkti = new List<string>();
-for (int i =1;i<=n;i++)
-{
-    Console.WriteLine("Vuvedete produkt");
-    List<string> produkt =Console.ReadLine().Split(' ').ToList();
-}
-for (int i =1;i<=n;i++)
-{
-    List<string> produkt = Console.ReadLine().Split(' ').ToList();
-    Console.WriteLine(i+"."+produkt);
-}
-
-
+int number = 384;
+string dignumber = Convert.ToString(number,2);
+Console.WriteLine(number+"(10)="+dignumber+"(2)");
