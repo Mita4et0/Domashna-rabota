f3fa2190e5dc417acebf3a1873dfb969d3f10a18
Mita4et0 <124554897+Mita4et0@users.noreply.github.com>
2023-12-14 15:03:54 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-string dignumber = "111011000";
-int number =Convert.ToInt32(dignumber,2);
-Console.WriteLine(dignumber + "(2)=" + number + "(10)");
+﻿Console.WriteLine("Vuvedete imeto na fitnesa");
+string name=Console.ReadLine();
+Console.WriteLine("Vuvedete ured");
+List<string> gym = Console.ReadLine().Split(' ').ToList();
+Console.WriteLine("Vuvedete ured na koito iskate da trenirate");
+string machine = Console.ReadLine();
+for(int i=0;i<1;i++)
+{
+    if (gym.Contains(machine)) 
+    Console.WriteLine("Da ima ured na koito iskash da trenirash");
+}
+for (int i = 0; i < gym.Count; i++)
+{
+    Console.WriteLine(gym[i]+"."+name);
+}
+gym.Add("legpresa");
+Console.WriteLine(gym.Count);
